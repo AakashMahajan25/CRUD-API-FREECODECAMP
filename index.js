d993aa34959d80bc2c30d873a517df3b41f7fb56
aakash <mahajanaakash108@gmail.com>
2024-02-28 17:27:52 +0530

    Progressed Further

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product.model.js');
+
 const app = express();
 app.use(express.json());
 
@@ -9,9 +11,32 @@ app.get('/', (req,res)=>{
     res.send('Hello from Node API');
 });
 
-app.post('/api/products', (req, res)=>{
-    console.log(req.body);
-    res.send(req.body);
+app.get('/api/products', async (req, res)=> {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.post('/api/products', async (req, res)=>{
+    try {
+        const createdProduct = await Product.create(req.body);
+        res.status(200).json(createdProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 })
 
 
@@ -21,6 +46,16 @@ mongoose.connect("mongodb+srv://mahajanaakash108:Qe9Z5hTptpNtsjE3@cluster0.quljz
     app.listen(3000, ()=>{
         console.log('Server is running on port 3000');
     });
+
+    app.post('/api/products', async (req, res)=>{
+    try {
+        const Product = await Product.create(req.body);
+        res.status(200).json(Product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
 })
 .catch(()=>{
     console.log("Connection Failed!")
